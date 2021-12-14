8002f2dcdb86ece679b3544746c86b91fd6fc1c0
HwangSungYeon <hsy0324@khu.ac.kr>
2021-12-15 02:49:51 +0900

    config file edited

--- yolov5/config.py
+++ yolov5/config.py
@@ -1,4 +1,4 @@
-AWS_ACCESS_KEY = "AKIAZZF3SST4PI2LQEK3"
-AWS_SECRET_ACCESS_KEY = "8/NnlXtIjsHVg54eLetZCmLQNluVgbvRhV38hu4O"
+AWS_ACCESS_KEY = "your_access_key"
+AWS_SECRET_ACCESS_KEY = "your_secret_key"
 AWS_S3_BUCKET_REGION = "ap-northeast-2"
-AWS_S3_BUCKET_NAME = "capstonedesignbucket"
+AWS_S3_BUCKET_NAME = "your_bucket"
